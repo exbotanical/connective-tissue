1af46cdb512570e9317a393936da7dbdbe2bad0e
goldmund <matthewtzito@gmail.com>
2021-06-08 10:06:00 -0700

    feat: add circular singly linked list

--- build/babel.config.js
+++ build/babel.config.js
@@ -1,16 +1,17 @@
 export default {
-  presets: [
-    [
-      '@babel/preset-env',
-      {
-        useBuiltIns: false,
-        modules: false
-      }
-    ]
-  ],
-  plugins: [
-    ['@babel/plugin-transform-runtime', {
-      regenerator: true
-    }]
-  ]
+	presets: [
+		['@babel/preset-env', {
+			shippedProposals: true,
+			useBuiltIns: false,
+			modules: false
+		}]
+	],
+	plugins: [
+		['@babel/plugin-proposal-class-properties', {
+			loose: false
+		}],
+		['@babel/plugin-transform-runtime', {
+			regenerator: true
+		}]
+	]
 }
